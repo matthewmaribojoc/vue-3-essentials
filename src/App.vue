02ab094f3274cd9fadd6f723052db190d60658cf
Matthew Maribojoc <matthewmaribojoc@gmail.com>
2021-02-26 11:17:22 -0500

    module 1.3

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
+  <Header title="my recipe" />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="My Vue 3 Project" />
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
+body {
+  margin: 0;
+  background-color: #fafafa;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <img src="../assets/recipe-logo.png" />
+      <h2>{{ formattedTitle }}</h2>
+    </div>
+    <nav>
+      <a href="#"> Home </a>
+      <a href="#" @click="loadRandomRecipe"> Random Recipe </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    loadRandomRecipe(evt) {
+      evt.preventDefault()
+      console.log('Load Random Recipe!')
+    },
+  },
+  computed: {
+    formattedTitle() {
+      let msg = this.title.split(' ')
+      msg = msg.map((word) => {
+        return word[0].toUpperCase() + word.substring(1)
+      })
+      return msg.join(' ')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.header {
+  background-color: #e67e22;
+  padding: 20px 5%;
+  margin: 0;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.logo img {
+  height: 100%;
+  margin-right: 5px;
+}
+
+.logo h2 {
+  color: white;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+  margin-right: 5px;
+}
+</style>
