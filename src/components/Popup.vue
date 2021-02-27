7b866e2db31d9e7dfb4303a35f98a43f42beab25
Matthew Maribojoc <matthewmaribojoc@gmail.com>
2021-02-27 18:56:02 -0500

    module 4.3

---
+++ src/components/Popup.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <teleport to="#teleport-target">
+      <div class="popup">
+        <div class="popup-content" ref="content">
+          <div class="close" @click="closePopup">X</div>
+          <slot> Popup Window Fallback </slot>
+          <!-- <iframe
+            width="560"
+            height="315"
+            frameborder="0"
+            allowfullscreen
+            :src="'https://youtube.com/embed/' + contentID"
+          /> -->
+        </div>
+      </div>
+    </teleport>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+export default {
+  setup(props, context) {
+    const content = ref(null)
+    const closePopup = () => {
+      context.emit('close')
+    }
+
+    onClickOutside(content, closePopup)
+
+    return {
+      closePopup,
+      content,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  position: relative;
+}
+
+.close {
+  background-color: #e74c3c;
+  color: white;
+  width: 16px;
+  height: 16px;
+  padding: 4px;
+  border-radius: 50%;
+  position: absolute;
+  left: -10px;
+  top: -10px;
+  text-align: center;
+  cursor: pointer;
+}
+</style>
--- src/components/YoutubePopup.vue
+++ src/components/YoutubePopup.vue
@@ -1,79 +1,27 @@
 <template>
-  <div>
-    <teleport to="#teleport-target">
-      <div class="popup">
-        <div class="popup-video" ref="video">
-          <div class="close" @click="closePopup">X</div>
-          <iframe
-            width="560"
-            height="315"
-            frameborder="0"
-            allowfullscreen
-            :src="'https://youtube.com/embed/' + videoID"
-          />
-        </div>
-      </div>
-    </teleport>
-  </div>
+  <popup>
+    <iframe
+      width="560"
+      height="315"
+      frameborder="0"
+      allowfullscreen
+      :src="'https://youtube.com/embed/' + videoID"
+    />
+  </popup>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { onClickOutside } from '@vueuse/core'
+import Popup from './Popup.vue'
+
 export default {
+  components: {
+    Popup,
+  },
   props: {
     videoID: {
       type: String,
       required: true,
     },
   },
-  setup(props, context) {
-    const video = ref(null)
-    const closePopup = () => {
-      context.emit('close')
-    }
-
-    onClickOutside(video, closePopup)
-
-    return {
-      closePopup,
-      video,
-    }
-  },
 }
 </script>
-
-<style scoped>
-.popup {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.popup-video {
-  background-color: white;
-  padding: 20px;
-  border-radius: 5px;
-  position: relative;
-}
-
-.close {
-  background-color: #e74c3c;
-  color: white;
-  width: 16px;
-  height: 16px;
-  padding: 4px;
-  border-radius: 50%;
-  position: absolute;
-  left: -10px;
-  top: -10px;
-  text-align: center;
-  cursor: pointer;
-}
-</style>
