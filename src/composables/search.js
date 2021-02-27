46021b288abfcd13c69f3d100d1d9fe2e777c5b9
Matthew Maribojoc <matthewmaribojoc@gmail.com>
2021-02-27 17:41:14 -0500

    module 3.5

--- src/composables/search.js
+++ src/composables/search.js
@@ -1,28 +1,44 @@
-import { ref } from 'vue'
+import { reactive, toRefs, onUpdated, watchEffect } from 'vue'
 
 export function useSearch() {
-  const query = ref('')
-  const currentQuery = ref('')
-  const meals = ref([])
+  const state = reactive({
+    query: '',
+    currentQuery: '',
+    meals: [],
+  })
+
+  onUpdated(() => {
+    console.log('updated')
+  })
+
+  let autocomplete
+  watchEffect(() => {
+    clearTimeout(autocomplete)
+    let query = state.query
+
+    autocomplete = setTimeout(() => {
+      console.log(query)
+    }, 1000)
+  })
 
   const search = () => {
-    if (query.value.trim().length == 0) {
-      meals.value = []
-      currentQuery.value = ''
+    if (state.query.trim().length == 0) {
+      state.meals = []
+      state.currentQuery = ''
       return
     }
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + query.value)
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + state.query)
       .then((response) => response.json())
       .then((data) => {
-        meals.value = data.meals
-        currentQuery.value = query.value
+        state.meals = data.meals
+        state.currentQuery = state.query
       })
   }
 
+  console.log(toRefs(state))
+
   return {
-    query,
-    currentQuery,
-    meals,
+    ...toRefs(state),
     search,
   }
 }
